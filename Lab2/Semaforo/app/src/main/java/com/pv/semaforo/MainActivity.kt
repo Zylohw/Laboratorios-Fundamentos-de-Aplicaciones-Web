7383420502ee86bdb6a1ac9576acf54990fd8aeb
Zylohw <pvasquez2206@gmail.com>
2026-02-01 20:44:35 -0600

    Add(Semaforo): se agregaron el diseño del componente y la logica de delay del semaforo

--- Lab2/Semaforo/app/src/main/java/com/pv/semaforo/MainActivity.kt
+++ Lab2/Semaforo/app/src/main/java/com/pv/semaforo/MainActivity.kt
@@ -15,6 +15,7 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.delay
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
