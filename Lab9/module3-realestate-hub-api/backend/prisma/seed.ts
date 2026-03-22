9b7d5f69a31aa9b113bcb9bc10676ec809ded2cd
Zylohw <pvasquez2206@gmail.com>
2026-03-21 22:21:47 -0600

    Update(Inicialization): importamos PrismaBetterSqlite3 y cse realizo un adapter para la incialización

--- Lab9/module3-realestate-hub-api/backend/prisma/seed.ts
+++ Lab9/module3-realestate-hub-api/backend/prisma/seed.ts
@@ -11,8 +11,11 @@
 // =============================================================================
 
 import { PrismaClient } from '@prisma/client';
+import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3';
 
-const prisma = new PrismaClient();
+
+const adapter = new PrismaBetterSqlite3({ url: 'file:./prisma/dev.db' });
+const prisma = new PrismaClient({adapter});
 
 // Datos de ejemplo (los mismos que en Module 2)
 const sampleProperties = [
